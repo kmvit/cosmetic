5bd9131c8105ebe42b729581c54bbf69cfdf1ed1
Андрей Галкин <justscoundrel@yandex.ru>
2024-09-11 12:47:50 +0300

    Add postgres

--- volkovcosmetics/settings.py
+++ volkovcosmetics/settings.py
@@ -88,6 +88,10 @@ DATABASES = {
     'default': {
         'ENGINE': env('ENGINE'),
         'NAME': env('NAME'),
+        'USER': env('USER'),
+        'PASSWORD': env('PASSWORD'),
+        'HOST': env('HOST'),
+        'PORT': env('PORT')
     }
 }
 
